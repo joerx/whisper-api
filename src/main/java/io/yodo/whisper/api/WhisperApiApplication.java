181031540dd1a82f99235c1d7dbb5d0302b46646
Joerg Henning <1712460+joerx@users.noreply.github.com>
2019-10-28 15:24:13 +0800

    Initial scaffold

---
+++ src/main/java/io/yodo/whisper/api/WhisperApiApplication.java
@@ -0,0 +1,13 @@
+package io.yodo.whisper.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhisperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhisperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yodo/whisper/api/WhisperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yodo.whisper.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhisperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
