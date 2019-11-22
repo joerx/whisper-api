1c7e256b3c442e75dd7f39bf04b11972c509a1c7
Joerg Henning <1712460+joerx@users.noreply.github.com>
2019-11-22 23:10:34 +0800

    Bump commons to 0.3

--- src/main/java/io/yodo/whisper/api/controller/ShoutController.java
+++ src/main/java/io/yodo/whisper/api/controller/ShoutController.java
@@ -2,6 +2,8 @@ package io.yodo.whisper.api.controller;
 
 import io.yodo.whisper.api.client.backend.WhisperBackendClient;
 import io.yodo.whisper.api.entity.ShoutPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +12,15 @@ public class ShoutController {
 
     private final WhisperBackendClient backend;
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     public ShoutController(WhisperBackendClient backend) {
         this.backend = backend;
     }
 
     @GetMapping("/shouts")
     public ShoutPage listShouts() {
+        log.debug("Requesting shouts from backend");
         return backend.getShouts();
     }
 }
--- src/main/java/io/yodo/whisper/api/controller/TokenController.java
+++ src/main/java/io/yodo/whisper/api/controller/TokenController.java
@@ -3,6 +3,8 @@ package io.yodo.whisper.api.controller;
 import io.yodo.whisper.api.client.backend.WhisperAuthClient;
 import io.yodo.whisper.api.entity.TokenRequest;
 import io.yodo.whisper.api.entity.TokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +14,15 @@ public class TokenController {
 
     private final WhisperAuthClient authClient;
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     public TokenController(WhisperAuthClient authClient) {
         this.authClient = authClient;
     }
 
     @PostMapping("/token")
     public TokenResponse getToken(@RequestBody TokenRequest tokenRequest) {
+        log.debug("Requesting token from auth service");
         return authClient.getToken(tokenRequest);
     }
 }
